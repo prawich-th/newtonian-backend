c1c4098f3cc2f18fd4e6b2815593dfaae128458b
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-10-16 05:26:04 +0700

    GET members and member info

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const routers = express.Router()
 const readerRoute = require("./reader")
 const eicRoute = require("./eic")
 const authRoute = require("./auth")
+const memberRoute = require("./member")
 
 
 routers.use('/reader', readerRoute)
 routers.use('/eic', eicRoute)
 routers.use('/auth', authRoute)
+routers.use('/member', memberRoute)
 
 
 module.exports = routers
--- models/db.js
+++ models/db.js
@@ -1,4 +1,6 @@
 const dotenv = require("dotenv").config();
+const { ObjectID } = require("bson");
+const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 const connectionString = process.env.ATLAS_URI;
 
@@ -13,11 +15,14 @@ db.once('connected', () => {
 const articlesSchema = new mongoose.Schema({})
 const issuesSchema = new mongoose.Schema({ no: Number })
 const eicsSchema = new mongoose.Schema({ username: String, password: String} )
+const memberSchema = new mongoose.Schema({ _id: ObjectId, name: String, position: Array, image: String, signature: String, articles: Array} )
+
 
 const Articles = mongoose.model("Articles", articlesSchema, "articles");
 const Issues = mongoose.model("Issues", issuesSchema, "issues")
 const Eics = mongoose.model("Eics", eicsSchema, "eics")
+const Members = mongoose.model("Members", memberSchema, "members")
 
 
 
-module.exports = { Articles, Issues, Eics }
+module.exports = { Articles, Issues, Eics, Members }
