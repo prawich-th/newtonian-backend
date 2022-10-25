c19eb90eb6c243a08fd67347b126d827ad0470c8
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-10-26 00:36:50 +0700

    get all articles

--- models/db.js
+++ models/db.js
@@ -12,7 +12,7 @@ db.once('connected', () => {
     console.log('Database Connected');
 })
 
-const articlesSchema = new mongoose.Schema({})
+const articlesSchema = new mongoose.Schema({ categories: String})
 const issuesSchema = new mongoose.Schema({ no: Number })
 const eicsSchema = new mongoose.Schema({ username: String, password: String} )
 const memberSchema = new mongoose.Schema({ _id: ObjectId, name: String, position: Array, image: String, signature: String, articles: Array} )
